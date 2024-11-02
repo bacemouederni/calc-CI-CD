3148d6754e5767b41e0ee07b8ea9726136bfeca1
bacem <bacemouederni@gmail.com>
2024-11-02 09:22:59 +0100

    modifier calc.py

--- sources/calc.py
+++ sources/calc.py
@@ -2,49 +2,48 @@
 La librairie calc permet de faire les opérations basiques de calcul entre deux entiers.
 """
 
-def add(arg1,arg2):
+def add(arg1: int, arg2: int) -> int:
     try:
-        return int(arg1)+int(arg2)
+        return int(arg1) + int(arg2)
     except ValueError: 
-        print("Vous devez entrer un entier.") 
+        return "Erreur : Vous devez entrer deux entiers."
 
-def sous(arg1,arg2):
+def sous(arg1: int, arg2: int) -> int:
     try:
-        return int(arg1)-int(arg2)
+        return int(arg1) - int(arg2)
     except ValueError: 
-        print("Un des arguments n'est pas un entier.") 
+        return "Erreur : Un des arguments n'est pas un entier."
 
-
-def mult(arg1,arg2):
+def mult(arg1: int, arg2: int) -> int:
     try:
-        return int(arg1)*int(arg2)
+        return int(arg1) * int(arg2)
     except ValueError: 
-        print("Un des arguments n'est pas un entier.") 
+        return "Erreur : Un des arguments n'est pas un entier."
 
-def div(arg1,arg2):
+def div(arg1: int, arg2: int) -> float:
     try:
-        return int(arg1)/int(arg2)
+        return int(arg1) / int(arg2)
     except ValueError: 
-        print("Un des arguments n'est pas un entier.") 
+        return "Erreur : Un des arguments n'est pas un entier."
     except ZeroDivisionError:
-        print("Vous divisez par 0.")
+        return "Erreur : Vous ne pouvez pas diviser par 0."
 
-def modulo(arg1,arg2):
+def modulo(arg1: int, arg2: int) -> int:
     try:
-        return int(arg1)%int(arg2)
+        return int(arg1) % int(arg2)
     except ValueError:
-        print("Un des arguments n'est pas un entier")    
+        return "Erreur : Un des arguments n'est pas un entier."
 
-def ope(operateur,arg1,arg2):   
-    if operateur=='+':
-        return add(arg1,arg2)        
-    elif operateur=="%":
-        return modulo(arg1,arg2)
-    elif operateur=="-":
-        return sous(arg1,arg2)
-    elif operateur=="x":
-        return mult(arg1,arg2)
-    elif operateur=="/":
-        return div(arg1,arg2)
+def ope(operateur: str, arg1: int, arg2: int):
+    if operateur == '+':
+        return add(arg1, arg2)        
+    elif operateur == '%':
+        return modulo(arg1, arg2)
+    elif operateur == '-':
+        return sous(arg1, arg2)
+    elif operateur == 'x' or operateur == '*':
+        return mult(arg1, arg2)
+    elif operateur == '/':
+        return div(arg1, arg2)
     else:
-        print("L'opérateur {} n'est pas reconnu.".format(operateur))
+        return f"Erreur : L'opérateur '{operateur}' n'est pas reconnu."
